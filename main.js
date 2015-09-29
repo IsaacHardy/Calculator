590631dfc508ce7a9a094e4a25da738229322325
Isaac Hardy <isaachardyDEV@gmail.com>
2015-09-29 17:06:45 -0400

    Added var declartions for each button - Removed highlighted outline from button:focus

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-var buttonOne = document.querySelector(".myButton.one");
+var clearBtn = document.querySelector('clear');
+var outputArea = document.querySelector('outputText')
 
-console.log(buttonOne);
+var sevenBtn = document.querySelector('seven');
+var eightBtn = document.querySelector('eight');
+var nineBtn = document.querySelector('nine');
+var divideBtn = document.querySelector('divide');
 
-buttonOne.addEventListener('click', function () {
-  buttonOne.style.color = "red";
-})
+var sixBtn = document.querySelector('six');
+var fiveBtn = document.querySelector('five');
+var fourBtn = document.querySelector('four');
+var multiplyBtn = document.querySelector('multiply');
+
+var threeBtn = document.querySelector('three');
+var twoBtn = document.querySelector('two');
+var oneBtn = document.querySelector('one');
+var minusBtn = document.querySelector('minus');
+
+var zeroBtn = document.querySelector('zero');
+var periodBtn = document.querySelector('period');
+var equalsBtn = document.querySelector('equals');
+var plusBtn = document.querySelector('plus');
+
+var firstInput = function () {
+  
+}
+
+var secondInput = function () {
+
+
+}
+
+equalsBtn.addEventListener('click', function () {
+  var calculate = Number(firstInput.value) + Number(secondInput.value);
+  outputArea.textContent = calculate;
+});
+
+
